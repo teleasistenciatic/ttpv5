ffec0ab8e8545ea73f9a6120e815a8182012cdf8
GAMO1J <josemanuel.galvez@magtel.es>
2015-06-01 15:06:30 +0200

    Modo Ducha:  - nueva opción de tiempo personalizado.  - incluida clase "duchaDialog" para mostrar diálogo con numberpicker.  - guardada en sharedpreferences la duración personalizada.

--- app/src/main/java/com/local/android/teleasistenciaticplus/act/ducha/actModoDucha.java
+++ app/src/main/java/com/local/android/teleasistenciaticplus/act/ducha/actModoDucha.java
@@ -1,24 +1,32 @@
 package com.local.android.teleasistenciaticplus.act.ducha;
 
-import android.app.Activity;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
 import com.local.android.teleasistenciaticplus.R;
 import com.local.android.teleasistenciaticplus.lib.helper.AppLog;
 import com.local.android.teleasistenciaticplus.lib.sound.PlaySound;
-import com.local.android.teleasistenciaticplus.lib.stats.StatsFileLogTextGenerator;
 import com.local.android.teleasistenciaticplus.modelo.Constants;
 
-public class actModoDucha extends Activity {
+public class actModoDucha extends FragmentActivity implements duchaDialog.duchaDialogNeutralListener {
+
+
+    private static int MinutosPersonalizado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_modo_ducha);
+
+
+        //TODO Recuperar los minutos personalizados del sharedpreferences
+
+        MinutosPersonalizado = 60;
+
 
         //TODO Comprobar antes de nada que haya contactos.
         // Si no hay mostrar diálogo para acceder a la zona de configuración.
@@ -26,28 +34,6 @@ public class actModoDucha extends Activity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_act_modo_ducha, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void activarDucha(View v){
         int tiempo_minutos = 0;
@@ -74,7 +60,7 @@ public class actModoDucha extends Activity {
         }
 
         Intent intent = new Intent(this, actDuchaCuentaAtras.class);
-        intent.putExtra("minutos",tiempo_minutos);
+        intent.putExtra("minutos", tiempo_minutos);
 
         startActivity(intent);
 
@@ -84,5 +70,31 @@ public class actModoDucha extends Activity {
 
         }
         finish();
+    }
+
+    public void elegirMinutos(View v){
+        duchaDialog newFragment = duchaDialog.newInstance(1,"Aceptar");
+        newFragment.show(getFragmentManager(),"dialog");
+
+    }
+    //Implementación del interfaz de diálogo
+    public void onAccionNeutral(DialogFragment dialog, int mins){
+
+
+        AppLog.i("DUCHA DIALOG", "Recibido: " + mins);
+
+
+        Intent intent = new Intent(this, actDuchaCuentaAtras.class);
+        intent.putExtra("minutos", mins);
+
+        startActivity(intent);
+
+        if( Constants.SHOW_ANIMATION ) {
+
+            overridePendingTransition(R.anim.animation2, R.anim.animation1);
+
+        }
+        finish();
+
     }
 }
